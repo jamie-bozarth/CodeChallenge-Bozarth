6fc5c323fadcb53f5846eb267c086c32a9c3fd3e
jamie-bozarth <68757617+jamie-bozarth@users.noreply.github.com>
2020-08-03 11:26:30 -0400

    Add files via upload

---
+++ code-challenge.Tests/ReportingStructureControllerTests.cs
@@ -0,0 +1,60 @@
+﻿using challenge.Controllers;
+using challenge.Data;
+using challenge.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using code_challenge.Tests.Integration.Extensions;
+
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using code_challenge.Tests.Integration.Helpers;
+using System.Text;
+
+namespace code_challenge.Tests.Integration
+{
+    [TestClass]
+    public class ReportingStructureControllerTests
+    {
+        private static HttpClient _httpClient;
+        private static TestServer _testServer;
+
+        [ClassInitialize]
+        public static void InitializeClass(TestContext context)
+        {
+            _testServer = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<TestServerStartup>()
+                .UseEnvironment("Development"));
+
+            _httpClient = _testServer.CreateClient();
+        }
+
+        [ClassCleanup]
+        public static void CleanUpTest()
+        {
+            _httpClient.Dispose();
+            _testServer.Dispose();
+        }
+        [TestMethod]
+        public void GetReportStructureById_Returns_Ok()
+        {
+            // Arrange
+            var employeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f";
+            var expectedReports = 4;
+
+
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/reportingstructure/{employeeId}");
+            var response = getRequestTask.Result;
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var new_reportingStructure = response.DeserializeContent<ReportingStructure>();
+            Assert.AreEqual(employeeId, new_reportingStructure.Employee.EmployeeId);
+            Assert.AreEqual(expectedReports, new_reportingStructure.NumberOfReports);
+        }
+    }
+}
