4a7270449d8aa646c7e4685feba8c17d52212e0f
jamie-bozarth <68757617+jamie-bozarth@users.noreply.github.com>
2020-08-03 11:25:42 -0400

    Add files via upload

---
+++ code-challenge.Tests/CompensationControllerTests.cs
@@ -0,0 +1,100 @@
+﻿using challenge.Controllers;
+using challenge.Data;
+using challenge.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using code_challenge.Tests.Integration.Extensions;
+
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using code_challenge.Tests.Integration.Helpers;
+using System.Text;
+
+namespace code_challenge.Tests.Integration
+{
+    [TestClass]
+    public class CompensationControllerTests
+    {
+        private static HttpClient _httpClient;
+        private static TestServer _testServer;
+
+        [ClassInitialize]
+        public static void InitializeClass(TestContext context)
+        {
+            _testServer = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<TestServerStartup>()
+                .UseEnvironment("Development"));
+
+            _httpClient = _testServer.CreateClient();
+        }
+
+        [ClassCleanup]
+        public static void CleanUpTest()
+        {
+            _httpClient.Dispose();
+            _testServer.Dispose();
+        }
+        [TestMethod]
+        public void CreateCompensation_Returns_Created()
+        {
+            // Arrange
+            var employeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f";
+            var expectedSalary = "100000";
+            var expectedEffectiveDate = Convert.ToDateTime("2017-09-08T19:01:55.714942+03:00");
+
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/employee/{employeeId}");
+            var employee_Response = getRequestTask.Result;            
+
+            var employee = employee_Response.DeserializeContent<Employee>();
+
+
+            Compensation compensation = new Compensation()
+            {
+                Employee = employee,
+                Salary = expectedSalary,
+                EffectiveDate = expectedEffectiveDate
+            };
+
+            var requestContent = new JsonSerialization().ToJson(compensation);
+            // Execute
+            var postRequestTask = _httpClient.PostAsync("api/compensation",
+               new StringContent(requestContent, Encoding.UTF8, "application/json"));
+            var response = postRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+
+            var newCompensation = response.DeserializeContent<Compensation>();
+            Assert.IsNotNull(newCompensation);
+            Assert.AreEqual(compensation.Employee.EmployeeId, newCompensation.Employee.EmployeeId);
+            Assert.AreEqual(compensation.Salary, newCompensation.Salary);
+            Assert.AreEqual(compensation.EffectiveDate, newCompensation.EffectiveDate);
+        }
+
+        [TestMethod]
+        public void GetCompensationById_Returns_Ok()
+        {
+            // Arrange
+            var employeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f";
+            var expectedSalary = "100000";
+            var expectedEffectiveDate = Convert.ToDateTime("2017-09-08T19:01:55.714942+03:00");
+
+
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/compensation/{employeeId}");
+            var response = getRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var new_Compensation = response.DeserializeContent<Compensation>();
+            Assert.AreEqual(expectedSalary, new_Compensation.Salary);
+            Assert.AreEqual(expectedEffectiveDate, new_Compensation.EffectiveDate);
+        }
+    }
+}
---
+++ code-challenge/Controllers/CompensationController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using challenge.Services;
+using challenge.Models;
+
+namespace challenge.Controllers
+{
+    [Route("api/compensation")]
+    public class CompensationController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly ICompensationService _compensationService;
+
+        public CompensationController(ILogger<CompensationController> logger, ICompensationService compensationServiceloyeeService)
+        {
+            _logger = logger;
+            _compensationService = compensationServiceloyeeService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCompensation([FromBody] Compensation compensation)
+        {
+            //_logger.LogDebug($"Received compensation create request for '{compensation.Employee.FirstName} {compensation.Employee.LastName}'");
+
+            _compensationService.Create(compensation);
+
+            return CreatedAtRoute("getCompensationById", new { id = compensation.Employee.EmployeeId }, compensation);
+        }
+
+        [HttpGet("{id}", Name = "getCompensationById")]
+        public IActionResult GetCompensationById(String id)
+        {
+            //_logger.LogDebug($"Received Compensation get request for '{id}'");
+
+            var compensation = _compensationService.GetById(id);
+
+            if (compensation == null)
+                return NotFound();
+
+            return Ok(compensation);
+        }
+    }
+}
